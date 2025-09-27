6368a83e85f5b8d2396c2e2602ff770661b20e6a
Xarch <rakhaarkhana98@gmail.com>
2025-09-27 22:24:57 +0700

    route login first

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Damaga",
+  description: "Damaga project reservastion",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {/* navbar / wrapper global bisa di sini */}
+        {children}
+      </body>
+    </html>
+  );
+}
