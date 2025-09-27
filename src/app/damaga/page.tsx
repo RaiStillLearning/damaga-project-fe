c659f5c40ef1a15b5ba15761e19b29ba016728c1
Xarch <rakhaarkhana98@gmail.com>
2025-09-27 23:16:33 +0700

    damaga 10% progress

--- src/app/damaga/page.tsx
+++ src/app/damaga/page.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
-export default function DamagaHome() {
+export default function Home() {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className="grid grid-cols-3 gap-4 p-6">
+      <div className="bg-red-300 rounded-lg shadow-xl h-40"></div>
+      <div className="bg-red-300 rounded-lg shadow-xl h-40"></div>
+      <div className="bg-red-300 rounded-lg shadow-xl h-40"></div>
+    </div>
   );
 }
